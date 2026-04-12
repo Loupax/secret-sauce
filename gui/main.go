7bb0f14bed29f2a8054c409bc40198e24a1572a9
Konstantinos Loupasakis <loupax@gmail.com>
2026-04-12 22:02:52 +0200

    feat(gui): add system tray menu with show and quit options

--- gui/main.go
+++ gui/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"embed"
 
+	"github.com/getlantern/systray"
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
 	"github.com/wailsapp/wails/v2/pkg/options/linux"
+	"github.com/wailsapp/wails/v2/pkg/runtime"
 )
 
 //go:embed all:frontend/dist
@@ -17,6 +19,31 @@ var icon []byte
 
 func main() {
 	app := NewApp()
+
+	// Run the system tray in a separate goroutine
+	go systray.Run(func() {
+		systray.SetIcon(icon)
+		systray.SetTitle("Secret Sauce")
+		systray.SetTooltip("Secret Sauce")
+
+		mShow := systray.AddMenuItem("Show Vault", "Show the Secret Sauce UI")
+		mQuit := systray.AddMenuItem("Quit", "Quit the whole app")
+
+		for {
+			select {
+			case <-mShow.ClickedCh:
+				if app.ctx != nil {
+					runtime.WindowShow(app.ctx)
+				}
+			case <-mQuit.ClickedCh:
+				if app.ctx != nil {
+					runtime.Quit(app.ctx)
+				}
+				systray.Quit()
+				return
+			}
+		}
+	}, func() {})
 
 	err := wails.Run(&options.App{
 		Title:  "Secret Sauce",
